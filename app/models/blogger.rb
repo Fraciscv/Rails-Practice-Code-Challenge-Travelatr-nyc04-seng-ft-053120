25aed3df8c6c8365dc6dfd6ec642d53c7fef78b2
Fraciscv <Francisco.Andaur@icloud.com>
2020-07-23 12:47:03 -0400

    Done.

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,24 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :posts
+    validates :name, uniqueness: true
+    validates :name, presence: true
+    validates :age, numericality: { greater_than: 0}
+    validates :bio, length: { minimum: 30}
 
+    def total_likes
+        self.posts.map { |post| post.likes}.sum
+    end
+
+    def featured_post
+        self.posts.order("likes").last
+    end
+
+    def top_destinations
+        hash = Hash.new(0)
+        self.destinations.each do |destination|
+            hash[destination] += destination.posts.count
+        end
+        hash.sort_by {|k,v| v}.map {|pair| pair[0]}.reverse[0..4]
+    end
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,19 @@
 class Destination < ApplicationRecord
+    has_many :posts
+    has_many :bloggers, through: :posts
+    
+    def five_most_recent_posts
+        self.posts.order("created_at desc").limit(5)
+    end
+
+    def featured_post
+        self.posts.order("likes").last
+    end
+
+    def average_blogger_age
+        total_bloggers = self.bloggers.uniq.count
+        sum_age = self.bloggers.uniq.map { |blogger| blogger.age }.sum
+        ((sum_age * 1.0) / total_bloggers).to_i
+    end
 
 end
